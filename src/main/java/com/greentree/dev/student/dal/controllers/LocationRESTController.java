d6b038eea4f41f1cc7947ce9cb6ee7496ce2eb43
savan <kansagrasavan108@gmail.com>
2022-05-31 15:57:49 +0530

    location REST controller completed

--- src/main/java/com/greentree/dev/student/dal/controllers/LocationRESTController.java
+++ src/main/java/com/greentree/dev/student/dal/controllers/LocationRESTController.java
@@ -3,10 +3,14 @@ package com.greentree.dev.student.dal.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.greentree.dev.student.dal.entites.Location;
@@ -28,4 +32,20 @@ public class LocationRESTController {
 	public Location createLocation(@RequestBody Location location) {
 		return locationRepository.save(location);
 	}
+	
+	@PutMapping
+	public Location updateLocation(@RequestBody Location location) {
+		return locationRepository.save(location);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteLocation(@PathVariable("id") Integer id) {
+		locationRepository.deleteById(id);
+	}
+	
+	@GetMapping("/{id}")
+	public Location getLocation(@PathVariable("id") int id) {
+		return locationRepository.findById(id).get();
+	}
+	
 }
